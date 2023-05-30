ff7ce8425a425096d0f1ceff741ae8c5f893708c
Marta Bianchini <marta.bianchini24@gmail.com>
2023-05-30 10:00:36 +0200

    refactored Artworks controller, added fetch object type function

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -5,17 +5,34 @@ class ArtworksController < ApplicationController
     # Empty the Artwork table
     Artwork.destroy_all
 
+    fetch_object_type("Construction toy")
+    fetch_object_type("Toy car")
+    fetch_object_type("Soft toy")
+    fetch_object_type("Educational toy")
+    fetch_object_type("Pull-along toy")
+    fetch_object_type("Puzzle")
+    fetch_object_type("Doll")
+    fetch_object_type("Mechanical toy")
+
+    @artworks = Artwork.all
+  end
+
+  private
+
+  def fetch_object_type(object_type)
     # API request:
-    response = HTTParty.get('https://api.vam.ac.uk/v2/objects/search?q_object_type="Construction toy"')
+    response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?q_object_type='#{object_type}'")
+    # Parsing the JSON result:
     artworks_data = JSON.parse(response.body)
-    # puts "*** Artworks data: #{artworks_data}\n\n" 
     artworks_info = artworks_data["info"]
-    # puts "*** artworks_info: #{artworks_info}\n\n"
+    puts "*** artworks_info: #{artworks_info} *** \n\n"
     artworks_records = artworks_data["records"]
-    puts "*** artworks_records index 2: #{artworks_records[2]} ***\n\n"
-    record_count = artworks_records.length
-    # record_count = artworks_info["record_count"]
-    # puts "There are #{record_count} object records that have the word 'Construction toy' in the object type"
+    record_count = artworks_info["record_count"]
+    image_count = artworks_info["image_count"]
+    puts "
+      There are #{record_count} object records that have the word 'Construction toy' in the object type\n 
+      But only #{image_count} images\n\n"
+  
     # puts "The first object is called '#{artworks_records[0]["_primaryTitle"]}' and has the type of '#{artworks_records[0]["objectType"]}'"
 
     i = 1
@@ -28,7 +45,7 @@ class ArtworksController < ApplicationController
 
         artwork = Artwork.new(
           index: i,
-          title: title.empty? ? "Construction Toy" : title,
+          title: title.empty? ? object_type : title,
           description: date.empty? ? "No date" : date,
           image_url: image_url,
         )
@@ -37,7 +54,5 @@ class ArtworksController < ApplicationController
         artwork.save
       end
     end
-
-    @artworks = Artwork.all
   end
 end
