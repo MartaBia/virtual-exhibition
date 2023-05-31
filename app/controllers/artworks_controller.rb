4374e58d8f408bdc5768618bdb160cbd01160a2f
Marta Bianchini <marta.bianchini24@gmail.com>
2023-05-31 21:49:21 +0200

    fist version of styling

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -2,7 +2,6 @@ require 'httparty'
 
 class ArtworksController < ApplicationController
   def index
-    # Empty the Artwork table
     Artwork.destroy_all
 
     object_types = [
@@ -26,20 +25,9 @@ class ArtworksController < ApplicationController
   private
 
   def fetch_object_type(object_type)
-    # API request:
     response = HTTParty.get("https://api.vam.ac.uk/v2/objects/search?q_object_type='#{object_type}'")
-    # Parsing the JSON result:
     artworks_data = JSON.parse(response.body)
-    artworks_info = artworks_data["info"]
-    puts "*** artworks_info: #{artworks_info} *** \n\n"
     artworks_records = artworks_data["records"]
-    record_count = artworks_info["record_count"]
-    image_count = artworks_info["image_count"]
-    puts "
-      There are #{record_count} object records that have the word 'Construction toy' in the object type\n 
-      But only #{image_count} images\n\n"
-  
-    # puts "The first object is called '#{artworks_records[0]["_primaryTitle"]}' and has the type of '#{artworks_records[0]["objectType"]}'"
 
     i = 1
     artworks_records.each do |artwork_record|
